3354a47f30c93bcaff42a15a1f466e8da055dbde
Josh Myers <josh.myers@headway.io>
2022-08-29 09:05:34 -0500

    adjust configs

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,28 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
+    presets: ['babel-preset-expo'],
     plugins: [
       [
-        "module:react-native-dotenv",
+        'module:react-native-dotenv',
         {
-          envName: "APP_ENV",
-          moduleName: "@env",
-          path: ".env",
+          envName: 'APP_ENV',
+          moduleName: '@env',
+          path: '.env',
           blacklist: null,
           whiltelist: null,
           safe: false,
           allowUndefined: false,
+        },
+      ],
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          extensions: ['.js', '.jsx', '.json', '.png', '.svg', '.ts', '.tsx'],
+          alias: {
+            src: './src',
+          },
         },
       ],
     ],
