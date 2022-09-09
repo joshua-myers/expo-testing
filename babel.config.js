ad796be5eb859a09cb210ac934629882d9ff8916
Josh Myers <josh.myers@headway.io>
2022-09-08 21:08:01 -0500

    adjust build script and app config

--- babel.config.js
+++ babel.config.js
@@ -11,7 +11,7 @@ module.exports = function (api) {
           path: '.env',
           blacklist: null,
           whiltelist: null,
-          safe: true,
+          safe: false,
           allowUndefined: false,
         },
       ],
